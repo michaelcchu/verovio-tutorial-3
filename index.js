9941ace6c6095e0486e77d44d9a1f806695a4ebe
michaelcchu <michael02chu@gmail.com>
2023-02-20 13:57:08 -0500

    removed midi functionality

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:audio/midi;base64,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-  
-    element.style.display = 'none';
-    document.body.appendChild(element);
-  
-    element.click();
-  
-    document.body.removeChild(element);
-  }
-
 function start() {
     const tk = new verovio.toolkit();
     console.log("Verovio has loaded!");
@@ -39,48 +26,6 @@ function start() {
     
         document.addEventListener("keydown", hightlightNextNote);    
     });
-
-
-
-    let currentPage = 1;
-
-    const midiHightlightingHandler = function (event) {
-        // Remove the attribute 'playing' of all notes previously playing
-        let playingNotes = document.querySelectorAll('g.note.playing');
-        for (let playingNote of playingNotes) playingNote.classList.remove("playing");
-        // Get elements at a time in milliseconds (time from the player is in seconds)
-        let currentElements = tk.getElementsAtTime(event.time * 1000);
-        if (currentElements.page == 0) return;
-        if (currentElements.page != currentPage) {
-            currentPage = currentElements.page;
-            document.getElementById("notation").innerHTML = tk.renderToSVG(currentPage);
-        }
-        // Get all notes playing and set the class
-        for (note of currentElements.notes) {
-            let noteElement = document.getElementById(note);
-            if (noteElement) noteElement.classList.add("playing");
-        }
-    }
-
-    MIDIjs.player_callback = midiHightlightingHandler;
-    
-    function saveMIDIHandler() {
-        let base64midi = tk.renderToMIDI();
-        download("output.mid",base64midi);
-    }
-    function playMIDIHandler() {
-        let base64midi = tk.renderToMIDI();
-        let midiString = 'data:audio/midi;base64,' + base64midi;
-        MIDIjs.play(midiString);
-    }
-
-    function stopMIDIHandler() {
-        MIDIjs.stop();
-    }
-
-    document.getElementById("saveMIDI").addEventListener("click", saveMIDIHandler);
-    document.getElementById("playMIDI").addEventListener("click", playMIDIHandler);
-    document.getElementById("stopMIDI").addEventListener("click", stopMIDIHandler);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
