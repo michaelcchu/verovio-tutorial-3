564ed5c1483b030fbca50ca2a60b26804e8f4cf5
michaelcchu <michael02chu@gmail.com>
2023-02-20 18:01:40 -0500

    got note-highlighting to work successfully

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-function start() {
+document.addEventListener("DOMContentLoaded", () => {
+    verovio.module.onRuntimeInitialized = process;
+});
+
+function process() {
     const tk = new verovio.toolkit();
-    console.log("Verovio has loaded!");
     tk.setOptions({
         svgAdditionalAttribute: ["note@pname", "note@oct"],
         breaks: "none"
     });
-    console.log("Verovio options:", tk.getOptions());
-    console.log("Verovio default options:", tk.getDefaultOptions());
-    fetch("https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei")
-    .then( (response) => response.text() )
-    .then( (meiXML) => {
-        tk.loadData(meiXML);
-        document.getElementById("notation").innerHTML += tk.renderToSVG(1); 
-        
-        const notes = document.getElementById("notation").querySelectorAll("g.note");
-        let i = -1;
 
-        function hightlightNextNote() {        
-            // Remove the attribute 'playing' of all notes previously playing
-            let playingNotes = document.querySelectorAll('g.note.playing');
-            for (let playingNote of playingNotes) playingNote.classList.remove("playing");
-            i++;
+    let notes; let i;
+
+    function readData(data) {
+        tk.loadZipDataBuffer(data);
+        document.getElementById("notation").innerHTML = tk.renderToSVG(1); 
+        notes = document.getElementById("notation").querySelectorAll("g.note");
+        i = -1;
+    }
+
+    input.addEventListener("change", () => {
+        for (const file of input.files) {
+            const reader = new FileReader();
+            reader.addEventListener("load", (e) => {readData(e.target.result)});
+            reader.readAsArrayBuffer(file);
+        }
+    });
+
+    fetch("Beethoven__Symphony_No._9__Op._125-Clarinetto_1_in_C_(Clarinet) - Copy - Copy (2).mxl")
+    .then( response => response.arrayBuffer() )
+    .then( data => {readData(data);} )
+    .catch( e => {console.log( e );} );
+
+    function hightlightNextNote() {     
+        // Remove the attribute 'playing' of all notes previously playing
+        let playingNotes = document.querySelectorAll('g.note.playing');
+        for (let playingNote of playingNotes) {
+            playingNote.classList.remove("playing");
+        }
+        i++;
+        if (i < notes.length) {
             notes[i].classList.add("playing");
         }
-    
-        document.addEventListener("keydown", hightlightNextNote);    
-    });
-}
+    }
 
-document.addEventListener("DOMContentLoaded", () => {
-    verovio.module.onRuntimeInitialized = start;
-})
+    document.addEventListener("keydown", hightlightNextNote);
+}
